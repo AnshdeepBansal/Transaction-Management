1442823209dfbdd5cafc04266058970384dee1f8
Anshdeep Bansal <149082227+AnshdeepBansal@users.noreply.github.com>
2025-03-09 17:22:03 +0530

    Update dbServer.js

--- backend/dbServer.js
+++ backend/dbServer.js
@@ -12,7 +12,7 @@ const supabaseKey = process.env.SUPABASE_KEY ;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(cors({
-  origin: "http://localhost:5173", // Changed elsewhere
+  origin: "https://transaction-management-cyan.vercel.app/", // Changed elsewhere
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"],
 }));
